8d592e5394946a6f3278f3407488586e0f059d20
Rifat Bhuiya <113363474+rifatbhuiya567@users.noreply.github.com>
2024-10-28 22:28:10 +0600

    responsive file

--- js/script.js
+++ js/script.js
@@ -17,10 +17,20 @@
         }
     });
     
+    // Searchbar
     $('.searchbar-btn').on('click', function () {
         $('.searchbar').toggle(300);
     });
+    
+    $('.mobile-searchbar').on('click', function () {
+        $('.searchbar').toggle(300);
+        $('.mobile-menu-items').hide(500);
+        $('.mobile-searchbar').toggleClass('active');
+        $('.mobile-menubar').removeClass('active');
+        $('.main-header').addClass('active-mobile-menu');
+    });
 
+    // Sub Menu Selector for Desktop
     $('.submenu-selector.one').on('mouseover', function () {
         $('.submenu-content.one').show(500);
         $('.submenu-selector.one').addClass('active');
@@ -35,12 +45,65 @@
         $('.submenu-selector.one').removeClass('active');
     });
 
+    // Mobile Menubar
+    $('.mobile-menubar').on('click', function () {
+        $('.mobile-menu-items').toggle(500);
+        $('.searchbar').hide(300);
+        $('.mobile-menubar').toggleClass('active');
+        $('.mobile-searchbar').removeClass('active');
+        $('.main-header').addClass('active-mobile-menu');
+    });
+
+    // Menu Dropdown for Mobile
+    $('.mobile-dropdown-btn.one').on('click', function () {
+        $('.mobile-dropdown-chld.one').toggle(500);
+        $('.mobile-dropdown-btn.one').toggleClass('active');
+    });
+
+    $('.mobile-dropdown-btn.two').on('click', function () {
+        $('.mobile-dropdown-chld.two').toggle(500);
+        $('.mobile-dropdown-btn.two').toggleClass('active');
+    });
+
+    $('.mobile-dropdown-btn.three').on('click', function () {
+        $('.mobile-dropdown-chld.three').toggle(500);
+        $('.mobile-dropdown-btn.there').toggleClass('active');
+    });
+
+    $('.mobile-dropdown-btn.four').on('click', function () {
+        $('.mobile-dropdown-chld.four').toggle(500);
+        $('.mobile-dropdown-btn.four').toggleClass('active');
+    });
+
+    $('.mobile-dropdown-btn.five').on('click', function () {
+        $('.mobile-dropdown-chld.five').toggle(500);
+        $('.mobile-dropdown-btn.five').toggleClass('active');
+    });
+
+    $('.mobile-dropdown-btn.six').on('click', function () {
+        $('.mobile-dropdown-chld.six').toggle(500);
+        $('.mobile-dropdown-btn.six').toggleClass('active');
+    });
+
+    // Sub Menu Selector for Mobile
+    $('.mobile-submenu-selector.one').on('click', function () {
+        $('.mobile-submenu.one').toggle(500);
+        $('.mobile-submenu-selector.one').toggleClass('active');
+    });
+
+    $('.mobile-submenu-selector.two').on('click', function () {
+        $('.mobile-submenu.two').toggle(500);
+        $('.mobile-submenu-selector.two').toggleClass('active');
+    });
+
+    // Back to Top
     $('.btt').on('click', function () {
         $('html, body').animate({
             scrollTop: 0
         }, 500);
-    })
+    });
 
+    // Get Year
     const d = new Date();
     let year = d.getFullYear();
     $("#currentYear").html(year);
